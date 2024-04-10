7bb9c62353e1a4c32391ff5ba845eeaee754ab85
mahalakshmi-bhogadi <85791488+mahalakshmi-bhogadi@users.noreply.github.com>
2024-04-10 04:07:34 +0000

    Rendered templates

--- app.py
+++ app.py
@@ -1,7 +1,41 @@
-from flask import Flask
+from flask import Flask,render_template,jsonify
 app=Flask(__name__)
+JOBS=[
+  {
+    'id':1 ,
+    'title':'Software Engineer',
+    'location':'Bengaluru, India',
+    'salary':'Rs. 10,00,000'},
+  {
+    'id':2 ,
+    'title':'Software Test Engineer',
+    'location':'Hyderabad, India',
+    'salary':'Rs. 8,00,000'
+  },
+  {
+    'id':3,
+    'title':'Data Analyst',
+    'location':'Australia',
+    'salary':'Rs. 15,00,000'
+  },
+  {
+    'id':4,
+    'title':'Data Scientist',
+    'location':'Chennai, India',
+    'salary':'Rs. 7,00,000'
+  },
+  {
+    'id':5,
+    'title':'Data Engineer',
+    'location':'Chennai, India'
+  }
+]
 @app.route("/")
 def maha():
-  return "Hello Maha"
+  return render_template("home.html",jobs=JOBS)
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+  
 if __name__=="__main__":
   app.run(host="0.0.0.0",debug=True)
